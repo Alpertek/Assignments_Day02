ac8ab8f87ab698188eea0b1b43f3c2723b12a04a
Alperen <80896036+Alpertek@users.noreply.github.com>
2021-03-18 15:12:49 +0300

    Add files via upload
    Answers to the Day2 Assignments

---
+++ Question3.java
@@ -0,0 +1,32 @@
+package assignments.day_2;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println("5");
+		System.out.println("6");
+		System.out.println("7");
+		System.out.println("8");
+		System.out.println("9");
+		System.out.println("10");
+		
+		// Alternative :) 
+		// System.out.print("1 \n");
+		// System.out.print("2 \n");
+		// System.out.print("3\n");
+		// System.out.print("4 \n");
+		// System.out.print("5 \n");
+		// System.out.print("6 \n");
+		// System.out.print("7 \n");
+		// System.out.print("8 \n");
+		// System.out.print("9 \n");
+		// System.out.print("10 \n");
+
+	}
+
+}
